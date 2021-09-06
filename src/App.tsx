06123bfca628785bd7a8acd77cade6c26c378d73
Jay Park <wngt3319@gmail.com>
2021-09-06 22:30:35 +0900

    working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
+import { css, keyframes } from '@emotion/react';
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+const headerStyles = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const appLogoStyles = css`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${appLogoSpin} infinite 20s linear;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div
+      css={css`
+        text-align: center;
+      `}
+    >
+      <header css={headerStyles}>
+        <img src={logo} css={appLogoStyles} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={css`
+            color: #61dafb;
+          `}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
