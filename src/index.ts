9fdb3f0323f3312ae05dd10e352f6d88cc73c546
Jay Park <wngt3319@gmail.com>
2021-09-08 00:03:36 +0900

    copy cra

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { program } from 'commander';
+import { Command } from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
+import { cwd } from 'process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import ora from 'ora';
 
-program.parse(process.argv);
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
+const program = new Command();
+const srcPath = path.resolve(__dirname, '../template');
+
+program.argument(`<dirName>`, `name of directory to create`).action(copyTemplate).parse();
+
+function copyTemplate(dirName: string) {
+  const spinner = ora('필요 파일 복사중...').start();
+  const destPath = path.resolve(cwd(), dirName);
+
+  fs.copySync(srcPath, destPath);
+
+  spinner.succeed('React 앱 생성 완료!');
+}
